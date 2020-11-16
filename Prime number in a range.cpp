9aa91e2c73fc1d0933e99a41bcdb2800e01578d1
Prasundas99 <prasundas115@gmail.com>
2020-11-16 14:01:29 +0530

    Prime number in range of two numbers

--- Prime number in a range.cpp
+++ Prime number in a range.cpp
@@ -2,20 +2,22 @@
 
 #include<iostream>
 using namespace std;
-void prime(int a){
-	int i,flag=0;
-	
-	for(i=0;i<a/2;i++){
-	  if(a%i==0){
-	  	flag = flag +1;
-	  }	
-	}
-	
-	if(flag==0){
-		cout<<a<<endl;
-	}
-	
+
+
+
+
+bool prime(int a){
+for(int i=2;i<=a/2;i++){
+if(a%i==0){
+return false;
 }
+}
+return true;
+}
+	
+	
+
+
 
 int main()
 { int i,n,m;
@@ -27,7 +29,8 @@ cout<<"second number :";
 cin>>m;
 
 	for(i=n;i<m+1;i++){
-		prime(i);
+		if(prime(i))
+		cout<<i<<endl;
 	}
 
 	

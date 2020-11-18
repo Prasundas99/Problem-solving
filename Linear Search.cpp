2377a103f269e883795bbfe6d86c642ca8d50308
Prasundas99 <prasundas115@gmail.com>
2020-11-18 19:17:03 +0530

    linear search

---
+++ Linear Search.cpp
@@ -0,0 +1,31 @@
+//Linear search
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,k;
+	
+	cin>>n;
+	
+	int a[n];
+	
+	for(i=0;i<n;i++){
+		cin>>a[i];
+	}
+
+	cout<<"Enter  number to search :"<<endl;
+	cin>>k;
+	
+	for(i=0;i<n;i++){
+		if(k == a[i]){
+			cout<<i<<endl;
+		}
+	}
+	
+	return 0;
+
+
+	
+return 0;	
+}

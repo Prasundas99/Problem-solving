8061b1a167e397d4ffdc3adc1af56d164a12d626
Prasundas99 <prasundas115@gmail.com>
2021-10-31 20:47:16 +0530

    Swapping 2 numbers

--- C Interview Questions/6. Swap Two Numbers.c
+++ C Interview Questions/6. Swap Two Numbers.c
@@ -1,1 +1,23 @@
-// C Program to Swap Two Numbers
+//C Program to Swap Two Numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	
+	printf("Input 2 numbers to swap:");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	printf("\n You entered 'a' = %d \n 'b'=%d \n ", a,b);
+	
+	int temp = a;
+	a = b;
+	b = temp;
+	
+	printf("You Swapped values \n 'a' = %d \n 'b'=%d \n ", a,b);
+	
+	
+	return 0;
+}

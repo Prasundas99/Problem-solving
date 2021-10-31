f13d45fe263a9668e6037126f45a373635c03b7c
Prasundas99 <prasundas115@gmail.com>
2021-10-31 20:55:09 +0530

    Even odd

---
+++ C Interview Questions/7.EvenOrOdd.c
@@ -0,0 +1,16 @@
+// Check Whether a Number is Even or Odd
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d", &num);
+	
+	
+	if(num % 2 == 0)
+	    printf("Even Number \n");
+	else
+	    printf("Odd Number \n");
+	
+	return 0;
+}
+

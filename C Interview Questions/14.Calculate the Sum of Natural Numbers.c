0b2e5510c830064093f09dd53decaea9ceef89a2
Prasundas99 <prasundas115@gmail.com>
2021-11-01 00:13:04 +0530

    Sum of natural numbers

---
+++ C Interview Questions/14.Calculate the Sum of Natural Numbers.c
@@ -0,0 +1,14 @@
+//C Program to Calculate the Sum of Natural Numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int sum=0;
+	for(int i=1;i<10;i++){
+	    sum = sum + i;
+	}
+	
+	printf("Sum of natural number is: %d", sum);
+	return 0;
+	return 0;
+}

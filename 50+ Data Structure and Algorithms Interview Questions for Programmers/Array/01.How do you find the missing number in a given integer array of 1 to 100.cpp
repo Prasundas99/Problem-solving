c72494cb91073793f9d9940f816c5c47e63aba2b
Prasundas99 <prasundas115@gmail.com>
2021-11-29 10:47:24 +0530

    DS started

---
+++ 50+ Data Structure and Algorithms Interview Questions for Programmers/Array/01.How do you find the missing number in a given integer array of 1 to 100.cpp
@@ -0,0 +1,14 @@
+/*
+Test Cases:
+        // one missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 6}, 6);
+ 
+        // two missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 6, 7, 9, 8, 10}, 10);
+ 
+        // three missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 6, 9, 8}, 10);
+ 
+        // four missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 9, 8}, 10);
+*/

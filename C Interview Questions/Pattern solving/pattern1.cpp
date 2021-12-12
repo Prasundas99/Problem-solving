e44d93d5ad10492c769d224706c2f07845299a33
Prasundas99 <prasundas115@gmail.com>
2021-12-12 20:42:26 +0530

    pattern questions added

---
+++ C Interview Questions/Pattern solving/pattern1.cpp
@@ -0,0 +1,60 @@
+/*
+****
+****
+****
+****
+
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+1111
+2222
+3333
+4444
+
+
+123
+456
+789
+
+
+
+X
+XX
+XXX
+XXXX
+XXXXX
+
+
+1
+2 3
+4 5 6
+7 8 9 10
+
+    X
+   XX
+  XXX
+ XXXX
+XXXXX
+
+
+   X
+  XXX
+ XXXXX
+XXXXXXX
+ XXXXX
+  XXX
+   X
+  
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	return 0;
+}

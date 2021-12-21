55034277410122b39e07844a28a97b5345816747
Prasundas99 <prasundas115@gmail.com>
2021-12-21 21:42:58 +0530

    Array operations

--- DS + DAA/04. Arrays/03.Operations on Arrays.cpp
+++ DS + DAA/04. Arrays/03.Operations on Arrays.cpp
@@ -8,6 +8,81 @@ Insertion − Adds an element at the given index.
 Deletion − Deletes an element at the given index.
 
 Search − Searches an element using the given index or by the value.
+*/
+#include <iostream>
+using namespace std;
 
-Update − Updates an element at the given index.
+int main() {
+	int n;
+	cin>>n;
+	
+	int arr[n];
+	for(int i=0; i<n;i++){
+	    cin>>arr[i];
+	}
+	//Traversing array
+	for(int i=0; i<n; i++){
+	    cout<<arr[i]<<endl;
+	}
+	
+	
+	
+	//Insertion
+	//Add element at given index
+	int a[10] = {9,19,29,39,49,59,69,79};
+    int k;
+	cin>>k;
+	int position;
+	cin>>position;
+	
+	int n=10, i=0;
+	for(i=n;i>position;i--){
+	    a[i] = a[i-1];
+	}
+	a[position] = k;
+	
+	//Traversing array
+	for(int i=0; i<n; i++){
+	    cout<<a[i]<<endl;
+	}
+	
+	
+	//Deletion
+	//Delete element at given index
+	int a[10] = {9,19,29,39,49,59,69,79};
+	int position;
+	cin>>position;
+	
+	int n=10, i=0;
+	for(i=position;i<n;i++){
+	    a[i] = a[i+1];
+	}
+
+	//Traversing array
+	for(int i=0; i<n; i++){
+	    cout<<a[i]<<endl;
+	}
+	
+	
+	//Search an element at given index
+	int b[10] = {1,2,3,4,5,6,7,8,9,10};
+	int n = 10;
+	int search;
+	cin>>search;
+	
+	for(int i=0; i<n; i++){
+	    if(b[i] == search){
+	        cout<<"Found Index is:"<<i;
+	        break;
+	    }
+	}
+	
+	return 0;
+}
+
+
+/*
+○	Arrays vs other DS
+i.e LinkedList and Array :  https://www.geeksforgeeks.org/linked-list-vs-array/
+
 */

0e8d3b8282d18b1317e3c7721e5797383a9de73b
Prasundas99 <prasundas115@gmail.com>
2021-12-21 16:00:53 +0530

    Dynamic memory allocation in cpp

--- DS + DAA/04. Arrays/02. Array Dynamic memory allocation.cpp
+++ DS + DAA/04. Arrays/02. Array Dynamic memory allocation.cpp
@@ -13,3 +13,36 @@ The array’s initial size and its growth factor determine its performance. Note
 If an array has a small size and a small growth factor, it will keep on reallocating memory more often. This will reduce the performance of the array.
 If an array has a large size and a large growth factor, it will have a huge chunk of unused memory. Due to this, resize operations may take longer. This will reduce the performance of the array.
 */
+
+
+/*
+In C++, memory is divided into two parts -
+
+Stack - All the variables that are declared inside any function take memory from the stack.
+Heap - It is unused memory in the program that is generally used for dynamic memory allocation.
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+cout<<"Enter a positive integer";
+int length;
+cin>>length;
+
+//syntax of new operator:  new data-type;
+int* array{new int[length]}; // use array new.  Note that length does not need to be constant!
+
+cout << "I just allocated an array of integers of length " << length << '\n';
+
+    array[0] = 5; // set element 0 to value 5
+
+    delete[] array; // use array delete to deallocate array
+
+    // we don't need to set array to nullptr/0 here because it's going to go out of scope immediately after this anyway
+	
+	return 0;
+}
---
+++ DS + DAA/04. Arrays/03.Operations on Arrays.cpp
@@ -0,0 +1,13 @@
+/*
+Following are the basic operations supported by an array.
+
+Traverse − print all the array elements one by one.
+
+Insertion − Adds an element at the given index.
+
+Deletion − Deletes an element at the given index.
+
+Search − Searches an element using the given index or by the value.
+
+Update − Updates an element at the given index.
+*/

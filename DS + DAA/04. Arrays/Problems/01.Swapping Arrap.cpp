356132cb7af1210222fff5921f53da1b3c9f3de3
Prasundas99 <prasundas115@gmail.com>
2021-12-22 23:45:14 +0530

    max element in array

--- DS + DAA/04. Arrays/Problems/01.Swapping Arrap.cpp
+++ DS + DAA/04. Arrays/Problems/01.Swapping Arrap.cpp
@@ -1,3 +1,18 @@
+/*
+Input: arr[] = { 1, 2, 3, 4, 5 } 
+Output: 2 1 5 3 4 
+Explanation: 
+Adjacent elements are swapped as follows: 
+(1, 2 -> 2, 1) 
+(3, 4, 5 -> 5, 3, 4)
+Input: arr[] = {1, 2, 3, 4} 
+Output: 2 1 4 3 
+Explanation: 
+Adjacent elements are swapped as follows: 
+1, 2 -> 2, 1 
+3, 4 -> 4, 3
+*/
+
 #include <iostream>
 using namespace std;
 //swaping values in array
@@ -5,20 +20,22 @@ void swapTwoNumber(int a,int b){
     int temp = a;
      a = b;
      b = temp; 
+          cout<<a<<" "<<b;
 }
 
-int swapThreeNumber(){
+
+int swapThreeNumber(int a, int b, int c){
     // Store sum of all in a
-    a = a + b + c;  // (a = 60)
+    a = a + b + c;  
  
     // After this, b has value of a
-    b = a - (b+c);  // (b = 60 – (20+30) =10)
+    b = a - (b+c);  
  
     // After this, c has value of b
-    c = a - (b+c);  // (c = 60 – (10 + 30) = 20)
+    c = a - (b+c);  
  
     // After this, a has value of c
-    a = a - (b+c);   //(a = 60 – (10 + 20) = 30)
+    a = a - (b+c);   
 }
 
 int main() {
---
+++ DS + DAA/04. Arrays/Problems/02.Max value in an array.cpp
@@ -0,0 +1,23 @@
+//Max value in an array
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+int a[n];
+
+for(int i=0; i< n ;i++){
+    cin>>a[i];
+}
+
+int max = a[0];
+for(int i=0 ; i<n;i++){
+    if(max<a[i])
+        max= a[i];
+}
+
+
+cout<<max<<endl;
+	return 0;
+}

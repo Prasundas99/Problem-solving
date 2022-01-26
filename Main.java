443028ade109a28b20808e5fc52f0b0defac9d68
Prasundas99 <prasundas115@gmail.com>
2022-01-26 20:25:21 +0530

    codeforce template

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);  
+            Integer a= sc.nextInt();
+            Integer b= sc.nextInt();
+            
+            System.out.println(b);
+        }
+}

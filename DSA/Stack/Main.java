6c916c7c3cf323803ddb8e561db61e0552bfc1a7
Prasundas99 <prasundas115@gmail.com>
2022-02-23 09:55:33 +0530

    feat: queue using collection

--- DSA/Stack/Main.java
+++ DSA/Stack/Main.java
@@ -18,29 +18,6 @@ public class Main {
         System.out.print(stack.pop());
 
         System.out.println("End of Collection Stack");
-        //Queue is an interfece not a function like linkedlist or stack
-        //So this queue is implemented using linkedlit
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
-        queue.add(4);
-        queue.add(5);
-        queue.add(6);
-       // Retrieves, but does not remove, the head of this queue,
-        System.out.print(queue.peek()); //returns 1
-        System.out.print(queue.remove()); //1 will be removed
-        System.out.println("End of Collection Queue");
-        //Deque
-        //a queue from ehichInsert and remove from both the side
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.add(6);
-        deque.addLast(9);
-        Integer element = deque.element();
-        System.out.println(element);
-
-
-        System.out.println("End of Collection Deque");
         //Cutom Stacks
         CustomStack stack2 = new CustomStack(50);
         stack2.push(69);
